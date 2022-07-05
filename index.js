8f1328f2a9994fa0c1e7f6821cc5e73a565dd317
Kevin Gicheha <gichehakevin@gmail.com>
2022-07-05 09:11:52 -0700

    commit tree 043b9186846bc7732487489a2f233350224be529

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers = ["Matt", "Kevin", "Nick", "nick"]
+function findMatching(arr, word) {
+
+    const newArr = arr.filter((element)=> {
+        if (element.toLowerCase() === word.toLowerCase()){
+            return true
+        } else {
+            return false
+        }
+    })
+    console.log(newArr)
+    return newArr;
+}
+findMatching(drivers, "Nick");
+findMatching(drivers, "matt");
